3a2be667b33e246d75117ab5cf2f6924fefb681e
wldnz <wildanofficial32@gmail.com>
2024-07-26 10:28:21 +0700

    Apus assets dan selesain merge conflict dgn index.html

--- frontend/assets/js/logout.js
+++
@@ -1,7 +0,0 @@
-let logoutButton = document.querySelector('.logout');
-logoutButton.onclick = () => {
-    if(window.confirm('yakin ingin logout?')){
-        localStorage.clear();
-        location.reload();
-    }
-}
